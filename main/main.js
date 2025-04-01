973b0fb8cfc8f1146e8b38ee33c7ad4cbbc0bedb
PsydoV2 <seb.falter@gmail.com>
2025-04-01 19:23:43 +0200

    beginn umstieg auf electron

---
+++ main/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  const startUrl =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:5173"
+      : `file://${path.join(__dirname, "../frontend/dist/index.html")}`;
+
+  win.loadURL(startUrl);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
